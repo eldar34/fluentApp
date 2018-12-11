c735051cb48dd76f02aa6912a0fb7de6ff52b99a
eldar <eldarphp@ya.ru>
2018-12-11 22:36:02 +0300

    Fix urls CB's

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Posts(models.Model):
 	title = models.CharField(max_length=150, db_index=True)
 	slug = models.SlugField(max_length=150, unique=True)
 	body = models.TextField(blank=True, db_index=True)
+	tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
 	date_pub = models.DateTimeField(auto_now_add=True)
 
 	def get_absolute_url(self):
@@ -14,3 +15,13 @@ class Posts(models.Model):
 	def __str__(self):
 		return '{}'.format(self.title)
 
+class Tag(models.Model):
+	title = models.CharField(max_length = 50)
+	slug = models.SlugField(max_length = 50, unique=True)
+
+	def __str__(self):
+		return '{}'.format(self.title)
+
+	def get_absolute_url(self):
+		return reverse('tag_detail_url', kwargs={'slug': self.slug})
+		
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import View
 
-from .models import Posts
+from .utils import ObjectDetailMixin 
+from .models import Posts, Tag
 
 # Create your views here.
 
@@ -10,7 +13,25 @@ def posts_list(request):
 	n = 'MaG'
 	return render(request, 'blog/index.html', context = {'posts': posts})
 
-def post_detail(request, slug):
-	post = Posts.objects.get(slug__iexact = slug)
-	return render(request, 'blog/post_detail.html', context = {'post': post})
+
+class PostDetail(ObjectDetailMixin, View):
+	model = Posts
+	template = 'blog/post_detail.html'
+	# def get(self, request, slug):
+	# 	post = get_object_or_404(Posts, slug__iexact=slug)
+	# 	return render(request, 'blog/post_detail.html', context = {'post': post})
+
+class TagDetail(ObjectDetailMixin, View):
+	model = Tag
+	template = 'blog/tag_detail.html'
+	# def get(self, request, slug):
+	# 	tag = get_object_or_404(Tag, slug__iexact = slug)
+	# 	return render(request, 'blog/tag_detail.html', context= {'tag': tag})
+		
+
+def tags_list(request):
+	tags = Tag.objects.all()
+	return render(request, 'blog/tags_list.html', context = {'tags': tags})
+
+
 
