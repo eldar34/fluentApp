4d85cbcca43e6b8cb62f4273a482e9f0cc8f3050
e.kerimov <e.kerimov@els24.com>
2018-12-12 20:12:31 +0300

    Added Tag Form

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import View
 
 from .utils import ObjectDetailMixin 
 from .models import Posts, Tag
+from .forms import TagForm
 
 # Create your views here.
 
@@ -21,13 +23,27 @@ class PostDetail(ObjectDetailMixin, View):
 	# 	post = get_object_or_404(Posts, slug__iexact=slug)
 	# 	return render(request, 'blog/post_detail.html', context = {'post': post})
 
+class TagCreate(View):
+	def get(self, request):
+		form = TagForm()
+		return render(request, 'blog/tag_create.html', context={'form': form})
+
+	def post(self, request):
+		bound_form = TagForm(request.POST)
+
+		if bound_form.is_valid():
+			new_tag = bound_form.save()
+			return redirect(new_tag)
+		return render(request, 'blog/tag_create.html', context = {'form': bound_form})
+
 class TagDetail(ObjectDetailMixin, View):
 	model = Tag
 	template = 'blog/tag_detail.html'
 	# def get(self, request, slug):
 	# 	tag = get_object_or_404(Tag, slug__iexact = slug)
 	# 	return render(request, 'blog/tag_detail.html', context= {'tag': tag})
-		
+
+
 
 def tags_list(request):
 	tags = Tag.objects.all()
